0d12d2170c473ff51d86c1ed49141d37a3097f4b
Kanishk Pachauri <itskanishkp.py@gmail.com>
2023-06-05 04:38:23 +0530

    Initial commit and Created Project template 😊

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bloom 🌺 : A Self-Hosted CFP portal for Events.
+
+Bloom - A project inspired by the natural process of flowers blooming and represents growth, development, and new opportunities.
+
+<p align="center">
+    <a href="bloom.fosscu.org">
+    <img width="72" height="72" src="https://user-images.githubusercontent.com/81439109/236648890-a2d9f527-1671-4b98-9efc-176955c82cdf.png" alt="Material Bread logo">
+    </a>
+</p>
---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <center>
+        <div>
+          <h1>We are Building BLOOM 🌺</h1>
+          <h3 className="under-child">
+            <a href="mailto: itskanishkp.py@gmail.com">
+              Contact Us for Help 😊
+            </a>
+            <p>
+              <a href="https://github.com/FOSS-Community/Bloom-frontend">
+                Contribute 🤝
+              </a>
+            </p>
+          </h3>
+        </div>
+      </center>
+    </>
+  );
+}
+export default App;
